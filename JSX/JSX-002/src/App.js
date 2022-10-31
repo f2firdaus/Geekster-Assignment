c722b89d36dc63017a1e5d2246287998eefa01a5
Firdaus Naseem <47087219+f2firdaus@users.noreply.github.com>
2022-10-31 19:15:27 +0530

    Add files via upload

---
+++ JSX/JSX-002/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.png';
+function App() {
+  return (
+    <>
+      <div className="innerbox">
+        <h1 className="h1">
+          The Evolution of <br /> Airbnb's Frontend
+        </h1>
+        <div className="bottom">
+          <h3>@spikerbrehm</h3>
+          <img src={logo} alt="" />
+</div>
+      </div>
+      
+     
+      
+      </>
+  );
+}
+
+export default App;
